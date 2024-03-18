ffb7c946547f238e7f80e00387ad4c7725ab8610
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2024-03-18 22:41:31 +0800

    chore: bump to 1.17.26 (#7)
    * chore: bump to 1.17.26
    
    * fixes
    
    * pre-run the build:docker step
    
    * fix ci.yml

--- packages/solana-container/version.js
+++ packages/solana-container/version.js
@@ -1,4 +1,4 @@
 // renovate: datasource=github-releases depName=solana-labs/solana
-const SOLANA_VERSION = '1.10.38';
+const SOLANA_VERSION = '1.17.26';
 
 module.exports = SOLANA_VERSION;
--- packages/solana-testcontainers/index.ts
+++ packages/solana-testcontainers/index.ts
@@ -1,7 +1,7 @@
 import { AbstractStartedContainer, GenericContainer, StartedTestContainer, Wait } from 'testcontainers';
 
 // renovate: datasource=github-releases depName=solana-labs/solana
-const SOLANA_VERSION = '1.10.38';
+const SOLANA_VERSION = '1.17.26';
 
 export class SolanaContainer extends GenericContainer {
   constructor(image: string = `ghcr.io/fuxingloh/solana-container:${SOLANA_VERSION}`) {
