0f81ebd7bf713f00fc00f84a1a6fcf1033ac9456
Fuxing Loh <git@fuxing.dev>
2024-06-11 18:34:10 +0800

    fix imports

--- packages/chainfile-solana/test-validator.test.ts
+++ packages/chainfile-solana/test-validator.test.ts
@@ -1,5 +1,5 @@
-import { afterAll, beforeAll, describe, expect, it } from '@jest/globals';
 import { ChainfileTestcontainers } from '@chainfile/testcontainers';
+import { afterAll, beforeAll, describe, expect, it } from '@jest/globals';
 
 import test from './test-validator.json';
 
